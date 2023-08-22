aad054f19a492c0eed537793612133ea69848308
Jaswanth Sai <jasvanthsai7@gmail.com>
2023-08-22 17:49:47 +0530

    starting project

---
+++ src/components/ingredients/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+import './Search.css';
+import Card from '../../ui/Card';
+
+const Search = React.memo(props => {
+  return (
+    <section className="search">
+      <Card>
+        <div className="search-input">
+          <label>Filter by Title</label>
+          <input type="text" />
+        </div>
+      </Card>
+    </section>
+  );
+});
+
+export default Search;
